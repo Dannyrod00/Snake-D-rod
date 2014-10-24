7315bdfacaec082645af284da040d877ef1f6d2e
Daniel Rodriguez <dannyrodriguez822@gmail.com>
2014-10-24 09:19:50 -0700

    10/24/2014

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -7,11 +7,14 @@ var screenWidth;
 var screenHeight;
 
 gameInitialize();
+snakeInitialize();
+gameDraw();
+snakeDraw();
 
 
 function gameInitialize(){
-    var canvas = document.getElementById("game-screen");
-    context = canvas.getContext("2d");
+    var canvas = document.getElementById ("game-screen");
+    context = canvas.getContext ("2d");
     
     screenWidth = window.innerWidth;
     screenHeight = window.innerHeight;
@@ -27,17 +30,17 @@ function gameLoop() {
 }
 
 function gameDraw() {
-    context.fillStyle = "rgb(167 ,0 ,176)";
-    context.fillRect(0, 0, screenWidth, screenHeight);
+    context.fillStyle = "rgb (167 ,0 ,176)";
+    context.fillRect (0, 0, screenWidth, screenHeight);
     
 }
 
 function snakeInitialize() {
     snake =[];  
-    snakeLength = 15;
-    snakeSize = 20;
+    snakeLength = 7;
+    snakeSize = 10;
     
-for (var index=  0; index < snakeLength; index++){
+for (var index=  snakeLength -1; index < snakeLength; index++){
     snake.push( {
         x: index,
         y: 0
@@ -49,7 +52,7 @@ for (var index=  0; index < snakeLength; index++){
 function snakeDraw() {
     for (var index=  0; index < snake.Length; index++){
         context.fillStyle = "white";
-        context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize,snakeSize,snakeSize);
+        context.fillRect (snake[index] .x * snakeSize, snake[index] .y * snakeSize, snakeSize, snakeSize);
         
     }
 }
