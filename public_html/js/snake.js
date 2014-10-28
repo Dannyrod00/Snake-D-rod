3df2c37c34550d653b647fc2ca84ffff2bcc9984
Daniel Rodriguez <dannyrodriguez822@gmail.com>
2014-10-28 09:19:24 -0700

    10/28/2014

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,18 +1,29 @@
-var snake;
-var snakeLength;
-var snakeSize;
-
-var context;
-var screenWidth;
-var screenHeight;
-
-gameInitialize();
-snakeInitialize();
-gameDraw();
-snakeDraw();
+         
+//these are my varables
+            
+            var snake;
+            var snakeLength;
+            var snakeSize;
+            var snakeDirection;
+            var food;
 
 
-function gameInitialize(){
+            var context;
+            var screenWidth;
+            var screenHeight;
+
+
+
+          gameInitialize();
+          snakeInitialize();
+          setInterval(gameLoop, 1000/10);
+          
+//these are my functions
+
+
+ function gameInitialize(){
+    
+    
     var canvas = document.getElementById ("game-screen");
     context = canvas.getContext ("2d");
     
@@ -25,12 +36,14 @@ function gameInitialize(){
 }
 
 function gameLoop() {
-    gameDraw();    
+   gameDraw();
+   snakeUpdate();
+   snakeDraw();
     
 }
 
 function gameDraw() {
-    context.fillStyle = "rgb (167 ,0 ,176)";
+    context.fillStyle = "rgb (255, 255, 255)";
     context.fillRect (0, 0, screenWidth, screenHeight);
     
 }
@@ -38,9 +51,10 @@ function gameDraw() {
 function snakeInitialize() {
     snake =[];  
     snakeLength = 7;
-    snakeSize = 10;
+    snakeSize = 30;
+    snakeDirection =- "down";
     
-for (var index=  snakeLength -1; index < snakeLength; index++){
+for (var index=  snakeLength -1; index >= 0; index--){
     snake.push( {
         x: index,
         y: 0
@@ -57,6 +71,41 @@ function snakeDraw() {
     }
 }
 
-function snakeUpdate() {
-    
-}
+//function snakeUpdate() {
+//var snakeHeadX =snake[0].x;
+//var snakeHeadY =snake[0].y;
+//
+//if( snakeDirection == "down" ){
+//    snakeHead++;
+//}
+//
+//else if( snakeDirection == "right" ){
+//    snakeHead++;
+//}
+//
+//var snakeTail = snake.pop();
+//snakeTail.x = snakeHeadx;
+//snakeTail.y = snakeHeady;
+//}
+//
+//function foodInitialize(){
+//food = {
+//    x:0,
+//    y:0        
+//
+//};
+//setFoodPositon();
+//}
+//
+//function foodDraw() {
+//contex.fillStyle = "white";
+//contex.fillRect = (food.x * snakeSize, food.y * snakeSize, snakeSize, snakeSize);
+//}
+//
+//function setFoodPosition() {
+//var randomX = Math.floor(Math.random() * screenWidth); 
+//var randomY = Math.floor(Math.random() * screenHeight);
+//
+//food.x = Math.floor(randomX / snakeSize);
+//food.y = Math.floor(randomY / snakeSize)
+//}
